87b7cf6da07b46ea598d36e3fffe9ebd855d4445
anvarinho <anvarinho@gmail.com>
2023-01-29 16:02:30 +0300

    views urls

--- website/app/urls.py
+++ website/app/urls.py
@@ -3,4 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('task/<str:pk>', views.task, name='task'),
+    path('offer/<str:pk>', views.offer, name='offer'),
+    path('makeoffer/<str:pk>', views.makeoffer, name='makeoffer'),
+    path('create/', views.create, name='create'),
+    path('main/', views.main, name='main'),
+    path('stuff/', views.stuff, name='stuff'),
+    path('become/', views.become, name='become'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+
+    path('ajax/load-list/', views.loadList, name='load-list')    
 ]
