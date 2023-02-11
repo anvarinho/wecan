e3b687dd98034fef637e3fa05d1a37af46e1e685
anvarinho <anvarinho@gmail.com>
2023-02-11 13:37:54 +0300

    url view of profile

--- website/app/api/views.py
+++ website/app/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.authtoken.models import Token
