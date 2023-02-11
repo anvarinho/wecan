3942e274ced641531409e05a926fbfc4a2d9b18d
anvarinho <anvarinho@gmail.com>
2023-02-11 13:55:25 +0300

    url view of profile

--- website/app/api/serializers.py
+++ website/app/api/serializers.py
@@ -16,7 +16,7 @@ class SubcategorySerializer(ModelSerializer):
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','username', 'first_name', 'last_name', 'avatar', 'registered']
+        fields = ['id','username', 'first_name', 'last_name', 'avatar', 'registered', 'bio']
 
 class TaskSerializer(ModelSerializer):
     client = UserSerializer(many=False, read_only=True)
